ed33a6ed586cd3de4dccd0fa8f09a18cbf625c2b
assane savadogo <assane.savadogo81@gmail.com>
2019-06-11 16:16:21 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,33 +1,7 @@
 def reformat_languages(languages)
-  new_hash = { 
-              :ruby => {
-   :type => "interpreted",
-   :style => [:oo]
-  },
-              :javascript => {
-    :type => "interpreted",
-    :style => [:oo, :functional]
-  },
-  :python => {
-    :type => "interpreted",
-    :style => [:oo]
-  },
-  :java => {
-    :type => "compiled",
-    :style => [:oo]
-  },
-  :clojure => {
-    :type => "compiled",
-    :style => [:functional]
-  },
-  :erlang => {
-    :type => "compiled",
-    :style => [:functional]
-  },
-  :scala => {
-    :type => "compiled",
-    :style => [:functional]
-  }
-}
-          
+  languages = { :ruby => "interpreted"
+                :type =>
+    
+  } 
+             
 end
