26d1d4ad30650a8d6d48a9e324be1f7d9a8135e8
assane savadogo <assane.savadogo81@gmail.com>
2019-06-11 16:05:37 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,8 @@
 def reformat_languages(languages)
-  new_hash = { :ruby => {
-    :type => "interpreted",
-    :style => [:oo]}
-  }
+  new_hash = { 
+              :ruby => {
+                        :type => "interpreted",
+                        :style => [:oo]}
+  },
+          
 end
